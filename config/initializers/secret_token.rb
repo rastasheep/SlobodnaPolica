8bc38aa03d1d63888be74b075e90a38de4f5bd74
Aleksandar Diklic <rastasheep3@gmail.com>
2012-04-02 23:37:22 +0200

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlobodnaPolica::Application.config.secret_token = '5647f18cf5689c4f12f63602b47431a59d67411f16ac1cbb68b1e29c009146b85d1afaa7748188126f666b86fa7b3bfc365530c27aa2b26e2a01f876d5bcb6fe'
