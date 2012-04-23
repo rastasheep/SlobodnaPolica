b5016fe509a73e4084a6b81b11ce864ff2945a0c
Aleksandar Diklic <rastasheep@gmail.com>
2012-04-23 03:04:59 +0200

    updated design

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,11 @@
 module BooksHelper
+	def title(page_title)
+		content_for (:title ) { page_title }
+	end
+	def yield_or_default(section, default = "")
+		content_for?(section) ? content_for(section) : default
+	end
+	
 	def categories
 		@cat = Array.new
 		categories = Book.select('distinct categories').collect(&:categories)
@@ -12,12 +19,5 @@ module BooksHelper
 	end
 	def authors
 		@auth = Book.select('distinct author').collect(&:author).sort!
-	end
-
-	def title(page_title)
-		content_for (:title ) { page_title }
-	end
-	def yield_or_default(section, default = "")
-		content_for?(section) ? content_for(section) : default
 	end
 end	
